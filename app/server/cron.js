8abfdf2e40d3098a44d531c7d36d3144e2ed1f7b
joshlgrossman <josh.l.grossman@gmail.com>
2017-05-19 06:21:05 -0500

    remove message deletion cronjob, file remains for future cronjobs

--- app/server/cron.js
+++ app/server/cron.js
@@ -1,13 +1,5 @@
 const schedule = require('node-schedule');
 const shell = require('./shell');
-const Message = require('./documents/Message');
-
-// at 4:00am, daily
-const deleteOldMessages = schedule.scheduleJob({minute: 0, hour: 4}, () => {
-  const twoDaysAgo = Date.now() - 172800000;
-  Message.Model.deleteMany({timestamp: {$lt: twoDaysAgo}});
-});
 
 module.exports = {
-  deleteOldMessages
 };
